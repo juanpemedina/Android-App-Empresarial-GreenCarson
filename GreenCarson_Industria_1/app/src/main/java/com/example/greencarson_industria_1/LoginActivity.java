196dbd0d892e22ad275b9828f87cdb8a7cbc5e1e
Juan Pedro Medina Silva <64823850+juanpemedina@users.noreply.github.com>
2023-11-10 21:01:21 -0600

    Configuracion Correcta del Login

--- GreenCarson_Industria_1/app/src/main/java/com/example/greencarson_industria_1/LoginActivity.java
+++ GreenCarson_Industria_1/app/src/main/java/com/example/greencarson_industria_1/LoginActivity.java
@@ -1,17 +1,91 @@
 package com.example.greencarson_industria_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //TextInputEditText editTextEmail, editTextPassword;
+    EditText editTextEmail, editTextPassword;
+    Button buttonLog;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword =  findViewById(R.id.password);
+
+        buttonLog = findViewById(R.id.btn_login);
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+                
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    //Cambiar a ingles
+                                    Toast.makeText(getApplicationContext(), "Login correctamente", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    //Cambiar a ingles
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
     }
     //Go to main
     public void goToMain(View v){
